61eb6a4ef6ebc20085cd6941ba9bee72f1c73726
evently-solutions <Bt34971!@#$%>
2020-03-29 21:21:42 -0700

    Adding timeline to connections page

--- lib/screens/connections_screen.dart
+++ lib/screens/connections_screen.dart
@@ -1,3 +1,4 @@
+import 'package:evently/widgets/timeline.dart';
 import 'package:flutter/material.dart';
 
 class ConnectionsScreen extends StatelessWidget {
@@ -5,6 +6,47 @@ class ConnectionsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final color = Colors.lightBlue;
+
+    return Container(
+      child: Timeline(
+        children: <Widget>[
+          Container(height: 100, color: color, child: Center(child: Text('Sample post', style: TextStyle(color: Colors.white),),),),
+          Container(height: 50, color: color, child: Center(child: Text('Sample event', style: TextStyle(color: Colors.white),),),),
+          Container(height: 200, color: color, child: Center(child: Text('Sample comment', style: TextStyle(color: Colors.white),),),),
+          Container(height: 100, color: color, child: Center(child: Text('Sample group post', style: TextStyle(color: Colors.white),),),),
+          Container(height: 100, color: color, child: Center(child: Text('Sample post', style: TextStyle(color: Colors.white),),),),
+          Container(height: 50, color: color, child: Center(child: Text('Sample event', style: TextStyle(color: Colors.white),),),),
+          Container(height: 200, color: color, child: Center(child: Text('Sample comment', style: TextStyle(color: Colors.white),),),),
+          Container(height: 100, color: color, child: Center(child: Text('Sample group post', style: TextStyle(color: Colors.white),),),),
+          Container(height: 100, color: color, child: Center(child: Text('Sample post', style: TextStyle(color: Colors.white),),),),
+          Container(height: 50, color: color, child: Center(child: Text('Sample event', style: TextStyle(color: Colors.white),),),),
+          Container(height: 200, color: color, child: Center(child: Text('Sample comment', style: TextStyle(color: Colors.white),),),),
+          Container(height: 100, color: color, child: Center(child: Text('Sample group post', style: TextStyle(color: Colors.white),),),),
+          Container(height: 100, color: color, child: Center(child: Text('Sample post', style: TextStyle(color: Colors.white),),),),
+          Container(height: 50, color: color, child: Center(child: Text('Sample event', style: TextStyle(color: Colors.white),),),),
+          Container(height: 200, color: color, child: Center(child: Text('Sample comment', style: TextStyle(color: Colors.white),),),),
+          Container(height: 100, color: color, child: Center(child: Text('Sample group post', style: TextStyle(color: Colors.white),),),),
+        ],
+        indicators: <Widget>[
+          Icon(Icons.person),
+          Icon(Icons.calendar_today),
+          Icon(Icons.note),
+          Icon(Icons.people),
+          Icon(Icons.person),
+          Icon(Icons.calendar_today),
+          Icon(Icons.note),
+          Icon(Icons.people),
+          Icon(Icons.person),
+          Icon(Icons.calendar_today),
+          Icon(Icons.note),
+          Icon(Icons.people),
+          Icon(Icons.person),
+          Icon(Icons.calendar_today),
+          Icon(Icons.note),
+          Icon(Icons.people),
+        ],
+      ),
+    );
   }
 }
